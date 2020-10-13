d05fe53ec67337ce31a9319fad2086ead019dcf9
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-10-13 18:06:06 +0300

    mugrations is work

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ import sys
 sys.path = ['', '..'] + sys.path[1:]
 from api.models import Base
 
-target_metadata = [Base.metadata]
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = [Base.metadata]
 
 
 # other values from the config, defined by the needs of env.py,
